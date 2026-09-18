ab86f03ccd440a1efa3f05a2343cc9eb35b7a854
mdtohidulislam34q-cyber <mdtohidulislam34q@gmail.com>
2026-09-18 23:03:27 +0600

    Add files via upload

---
+++ Site_config.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Config;
+
+class Site_config extends \CodeIgniter\Config\BaseConfig
+{
+	public $site_title = 'ZIN PAY';
+	public $site_keywords = 'zinpay.site';
+	public $site_description = 'ZIN PAY is your one-stop platform for seamless and secure payment automation. We specialise in streamlining payment processes for businesses and individuals, offering features like recurring billing, real-time transaction tracking, and integration with multiple payment gateways. With a focus on efficiency and user-friendliness, Auto Pay Solution ensures your payments are handled effortlessly, giving you more time to focus on growth. Experience the convenience of automated payments with our cutting-edge technology and reliable customer support.';
+	public $optimize = '1';
+	public $site_logo = 'public/uploads/admin/356a192b7913b04c54574d18c28d46e6395428ab/1775049592_aa519736251305f071c5.png';
+	public $site_icon = 'public/uploads/admin/356a192b7913b04c54574d18c28d46e6395428ab/1775049614_92c7f55eff2c6f78a493.png';
+	public $site_name = 'ZIN PAY';
+	public $update_file = '1';
+	public $is_maintenance_mode = '0';
+	public $maintenance_mode_time = '2024-06-02T20:31';
+	public $enable_https = '1';
+	public $address = 'Dhaka,BD';
+	public $social_facebook_link = 'https://www.facebook.com/';
+	public $social_instagram_link = '#';
+	public $social_pinterest_link = '#';
+	public $social_twitter_link = '#';
+	public $social_tumblr_link = '';
+	public $social_youtube_link = '#';
+	public $contact_tel = '+880(WhatsApp)';
+	public $contact_email = 'mail@zinpay.zite';
+	public $contact_work_hour = 'Sat-Thu 09 am - 10 pm';
+	public $copy_right_content = 'zinpay.site';
+	public $honeypot = '';
+	public $app_link = '';
+	public $payment_url = '';
+	public $verify_url = '';
+	public $social_fecebook_link = '';
+	public $social_github_link = '#';
+	public $work_hour = '';
+	public $contacr_work_hour = '';
+	public $google_login = '1';
+	public $google_auth_clientId = 'Google Auth Client Id';
+	public $google_auth_clientSecret = 'Google Auth ClientSecret';
+	public $enable_goolge_translator = '0';
+	public $enable_goolge_recapcha = '0';
+	public $google_capcha_site_key = '';
+	public $google_capcha_secret_key = '';
+	public $enable_kyc = '1';
+	public $preoloader = '1';
+	public $preloader = '0';
+	public $enable_google_translator = '1';
+	public $enable_google_recapcha = '1';
+	public $enable_google_recaptcha = '0';
+}
